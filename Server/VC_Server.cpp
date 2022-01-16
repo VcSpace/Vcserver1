a880107effb7858700e304047a2b799543209cfb
Vcvc <vcvckw@gmail.com>
2022-01-16 23:56:51 +0800

    server

--- Server/VC_Server.cpp
+++ Server/VC_Server.cpp
@@ -5,7 +5,8 @@ namespace vc
     Vc_Server::~Vc_Server() {
 
     }
-    Vc_Server::Vc_Server(const std::string &ip, const int &port) : m_ip(ip), m_port(port)
+    Vc_Server::Vc_Server(const std::string &ip, const int &port, const int &epollevent, const int &maxconn, bool et) : m_ip(ip), m_port(port),
+                                                                                                    _epollevent(epollevent), _maxconnect(maxconn), _et(et)
     {
 
     }
@@ -18,6 +19,7 @@ namespace vc
         if(_Sock.Create_socket(Domain, type))
         {
             std::cout << "CreateSocket" << std::endl;
+            m_sock = _Sock.getfd();
         }
         else
         {
@@ -40,5 +42,57 @@ namespace vc
         }
     }
 
+    void Vc_Server::CreateEpoll()
+    {
+        _epoller.create_epoll(_epollevent, _maxconnect, _et); //event, conn, et
+
+        _epoller.add(_Sock.getfd(), EPOLLIN);
+
+        std::cout << "create_epoll success" << std::endl;
+    }
+
+    void Vc_Server::run()
+    {
+        std::cout << "server run" << std::endl;
+
+        int i = 0;
+        while(true)
+        {
+            int _ep_wait = _epoller.wait();
+
+            for(i = 0; i < _ep_wait; ++i)
+            {
+                if(_ep_wait == m_sock)
+                {
+                    NewConnection();
+                }
+            }
+        }
+
+    }
+
+    void Vc_Server::NewConnection()
+    {
+        struct sockaddr_in client_addr;
+        socklen_t client_addr_len = sizeof(client_addr);
+
+        if(_et)
+        {
+            while(true)
+            {
+                int connfd = accept(m_sock, (struct sockaddr *) &client_addr, &client_addr_len);
+                if(connfd < 0)
+                {
+                    std::cout << "accept error" << std::endl;
+                    break;
+                }
+                else
+                {
+                    ;
+                }
+            }
+        }
+
+    }
 
 } //vc
--- Server/VC_Server.h
+++ Server/VC_Server.h
@@ -1,29 +1,36 @@
 #ifndef VCSERVER_VC_SERVER_H
 #define VCSERVER_VC_SERVER_H
 
-
 #include "VC_Socket.h"
+#include "../Epoll/VC_Epoller.h"
 
 namespace vc
 {
     class Vc_Server
     {
     public:
-        Vc_Server(const std::string &ip, const int &port);
+        Vc_Server(const std::string &ip, const int &port, const int &epollevent, const int &maxcon, bool et);
         ~Vc_Server();
 
         void CreateSocket();
+        void CreateEpoll();
         void bind();
+        void run();
+        void NewConnection();
 
     private:
         Vc_Socket _Sock;
+        VC_Epoller _epoller;
 
     private:
         int m_sock;
         int fd;
+        int m_port;
+        int _epollevent;
+        int _maxconnect;
 
         std::string m_ip;
-        int m_port;
+        bool _et;
     };
 
 } //vc
