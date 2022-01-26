82c2f04cb246b688027152cacdd7cdec99a854ba
Vcvc <vcvckw@gmail.com>
2022-01-26 22:43:34 +0800

    timer

--- Timer/VC_Timer.cpp
+++ Timer/VC_Timer.cpp
@@ -1,1 +1,21 @@
 #include "VC_Timer.h"
+
+namespace vc
+{
+    VC_Timer::VC_Timer(int maxconn, int time) : _times(new vc::TimerNode[maxconn])
+    {
+
+    }
+
+    VC_Timer::~VC_Timer()
+    {
+        if(_times)
+        {
+            delete _times;
+        }
+
+        _times = nullptr;
+    }
+
+
+} //vc
--- Timer/VC_Timer.h
+++ Timer/VC_Timer.h
@@ -1,10 +1,34 @@
 #ifndef VCSERVER_VC_TIMER_H
 #define VCSERVER_VC_TIMER_H
 
+#include <chrono>
+#include <time.h>
+#include <cassert>
+#include <functional>
+#include <vector>
 
-class VC_Timer {
+namespace vc
+{
+    struct TimerNode {
+        int id;
+        int expires;
+        bool _istimeout = false;
 
-};
+        bool operator<(const TimerNode& t) {
+            return expires < t.expires;
+        }
+    };
 
+    class VC_Timer
+    {
+    public:
+        VC_Timer(int maxconn, int time);
+        ~VC_Timer();
+
+    private:
+        struct TimerNode *_times;
+
+    };
+} //vc
 
 #endif //VCSERVER_VC_TIMER_H
