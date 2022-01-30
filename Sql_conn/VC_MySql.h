3ca2e7f001b345d20508b0dcf67b92092418f38b
Vcvc <vcvckw@gmail.com>
2022-01-30 23:14:57 +0800

    sql

--- Sql_conn/VC_MySql.h
+++ Sql_conn/VC_MySql.h
@@ -1,10 +1,30 @@
 #ifndef VCSERVER_VC_MYSQL_H
 #define VCSERVER_VC_MYSQL_H
 
+#include <mysql/mysql.h>
+#include <string>
 
-class VC_MySql {
+namespace vc
+{
 
-};
+    class VC_Mysql
+    {
+    public:
+        VC_Mysql();
+        ~VC_Mysql();
 
+        void init_sql();
+        void conn_sql();
+        void client_login();
+        void client_join();
+
+    private:
+        std::string sql_username;
+        std::string sql_password;
+        std::string sql_database;
+        std::string sql_table;
+
+    };
+} //vc
 
 #endif //VCSERVER_VC_MYSQL_H
