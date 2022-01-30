675438ba5bc8bda5f8b6971e836ce8741a019adc
Vcvc <vcvckw@gmail.com>
2022-01-30 23:16:11 +0800

    sql

--- Sql_conn/VC_MySql.cpp
+++ Sql_conn/VC_MySql.cpp
@@ -1,1 +1,29 @@
 #include "VC_MySql.h"
+
+namespace vc
+{
+
+    VC_Mysql::VC_Mysql() {
+
+    }
+
+    VC_Mysql::~VC_Mysql() {
+
+    }
+
+    void VC_Mysql::init_sql() {
+
+    }
+
+    void VC_Mysql::conn_sql() {
+
+    }
+
+    void VC_Mysql::client_login() {
+
+    }
+
+    void VC_Mysql::client_join() {
+
+    }
+} //vc
